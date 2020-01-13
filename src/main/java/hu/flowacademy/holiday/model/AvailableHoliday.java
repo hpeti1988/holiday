ed3c11e1f4deb441550b8800ac916da52527710c
Peter Horvath <hpeti1988@gmail.com>
2020-01-13 14:03:26 +0100

    continued filling up classes

--- src/main/java/hu/flowacademy/holiday/model/AvailableHoliday.java
+++ src/main/java/hu/flowacademy/holiday/model/AvailableHoliday.java
@@ -1,4 +1,26 @@
 package hu.flowacademy.holiday.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvailableHoliday {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ID;
+    @OneToOne
+    private User user;
+    private int countOfHoliday = 20;
+    private int extraHoliday = 0;
 }
+
--- src/main/java/hu/flowacademy/holiday/model/Holiday.java
+++ src/main/java/hu/flowacademy/holiday/model/Holiday.java
@@ -1,4 +1,36 @@
 package hu.flowacademy.holiday.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Holiday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+    private String boss;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private HolidayType type;
+    private StatusType status;
+
+    public enum HolidayType {
+        PTO, VTO, SICK
+    }
+
+    public enum StatusType {
+        APPROVED, DECLINED, PENDING
+    }
 }
